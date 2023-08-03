cf9a686074711448846fc072cfa27fbba5a299f2
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-08-03 21:48:00 +0500

    Set up initial classes and event listeners for shopping cart functionality.

---
+++ script.js
@@ -0,0 +1,48 @@
+const cartBtn = document.querySelector(".cart-btn");
+const closeBtn = document.querySelector(".close-btn");
+const clearCartBtn = document.querySelector(".cleart-cart");
+const cartDOM = document.querySelector(".cart");
+const cartOverlay = document.querySelector(".cart-overlay");
+const cartItems = document.querySelector(".cart-items");
+const cartTotal = document.querySelector(".cart-total");
+const cartContent = document.querySelector(".cart-content");
+const productsDOM = document.querySelector(".products-center");
+
+// cart
+const cart = [];
+
+// getting the products
+class Products {
+  async getProducts() {
+    try {
+      let response = await fetch("products.json");
+      let data = await response.json();
+      let products = data.items;
+      products = products.map((item) => {
+        const { title, price } = item.fields;
+        const { id } = item.sys;
+        const image = item.fields.image.fields.file.url;
+        return {title, price, id, image};
+      })
+      return products;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+
+// displaying products
+class UI {}
+
+
+// local storage
+class Storage {}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const uI = new UI();
+  const products = new Products();
+
+  // get all products
+  products.getProducts().then((data) => console.log(data));
+});
